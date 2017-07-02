4732f4f8c448091189624f15ca812931dd14e78a
Steven Jimenez <stevenmjimenez@gmail.com>
2017-07-02 15:04:17 -0400

    adds Interface class to code-base

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# js-singly-linked-list
-Singly Linked List example code
+# JavaScript List Examples
+This repository contains example code for JavaScript Lists
+
+## Source Code ##
+* src/interface.js : JavaScript class that emulates an object interface
+
+## Distribute Code ##
+* dist/interface[.min].js : JavaScript interface class
---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+var gulp    = require('gulp'),
+    uglify  = require('gulp-uglify'),
+    rename  = require('gulp-rename'),
+    concat  = require('gulp-concat');
+
+var tasks = {
+    'interface': function() {
+        return gulp.src('src/interface.js')
+            .pipe(concat('interface.js'))
+            .pipe(gulp.dest('dist'))
+            .pipe(rename({ suffix: '.min' }))
+            .pipe(uglify())
+            .pipe(gulp.dest('dist'));
+    },
+};
+
+for (var task in tasks) {
+    gulp.task(task, tasks[task]);
+}
+
+gulp.task('default', [], function() {
+    gulp.start(...Object.keys(tasks));
+});
